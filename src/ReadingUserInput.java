80c00eefd134631a7f5c235892f763a58c7a8e2e
Kaung-Htet <kaunghtetnyilay@gmail.com>
2020-07-12 15:54:49 +0700

    Initial commit

---
+++ src/ReadingUserInput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ReadingUserInput {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter your year of birth:");
+        int yearOfBirth = scanner.nextInt();
+        int age = 2020 - yearOfBirth;
+
+        System.out.println("Enter your name:");
+        String name = scanner.nextLine();
+
+        System.out.println("Your name is " + name + " and you are " + age + " years old");
+    }
+}
