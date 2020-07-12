44e45cafc2b6897dc6f72bacd0adc1085844eae6
Kaung-Htet <kaunghtetnyilay@gmail.com>
2020-07-12 22:20:53 +0700

    Add valid number check code

--- src/ReadingUserInput.java
+++ src/ReadingUserInput.java
@@ -5,25 +5,43 @@ public class ReadingUserInput {
         Scanner scanner = new Scanner(System.in);
         
         System.out.println("Enter your year of birth:");
-        int yearOfBirth = scanner.nextInt();
-        int age = 2020 - yearOfBirth;
 
-        //အောက်ကလိုင်းမပါရင် နောက်တစ်ကြိမ် နာမည် input ကုဒ်က input မတောင်းတော့ဘူး
-        //whenever we relate a number from the scanner, being an Int or Float or Double or some other type,
-        //we press the Enter key on the keyboard as we just did with the age.
-        //So doing that ends the line
-        //to handle the Enter key issue, we have to call next line, so that the scanner works as expected
-        //Now internally the scanner is checking for a line separator
-        //when we hit Enter we're typing a line separator so that's interpreted as input
-        //When we reach the next line method that retrieves the name
-        //the input for a name becomes Enter, so is essentially skipped.
-        scanner.nextLine(); //handle next line character
+        //return true if user input is int
+        boolean hasNextInt = scanner.hasNextInt();
 
-        System.out.println("Enter your name:");
-        String name = scanner.nextLine();
+        if (hasNextInt){
 
-        System.out.println("Your name is " + name + " and you are " + age + " years old");
+            int yearOfBirth = scanner.nextInt();
 
-        scanner.close();
+            //အောက်ကလိုင်းမပါရင် နောက်တစ်ကြိမ် နာမည် input ကုဒ်က input မတောင်းတော့ဘူး
+            //whenever we relate a number from the scanner, being an Int or Float or Double or some other type,
+            //we press the Enter key on the keyboard as we just did with the age.
+            //So doing that ends the line
+            //to handle the Enter key issue, we have to call next line, so that the scanner works as expected
+            //Now internally the scanner is checking for a line separator
+            //when we hit Enter we're typing a line separator so that's interpreted as input
+            //When we reach the next line method that retrieves the name
+            //the input for a name becomes Enter, so is essentially skipped.
+            scanner.nextLine(); //handle next line character (Enter)
+
+            int age = 2020 - yearOfBirth;
+
+            System.out.println("Enter your name:");
+            String name = scanner.nextLine();
+
+            if (age >= 0 && age <= 100){
+                System.out.println("Your name is " + name + " and you are " + age + " years old");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+
+            //It's recommended that we close the scanner after we don’t need it anymore
+            // as it then releases the underlying memory that scanner was using internally.
+            scanner.close();
+        } else {
+            System.out.println("Unable to parse year of birth");
+        }
+
+
     }
 }
