9e8876b7eaadfa42ec27c678e2a708b58ad429e7
Kaung-Htet <kaunghtetnyilay@gmail.com>
2020-07-12 16:30:21 +0700

    Add comments and some code

--- src/ReadingUserInput.java
+++ src/ReadingUserInput.java
@@ -8,9 +8,22 @@ public class ReadingUserInput {
         int yearOfBirth = scanner.nextInt();
         int age = 2020 - yearOfBirth;
 
+        //အောက်ကလိုင်းမပါရင် နောက်တစ်ကြိမ် နာမည် input ကုဒ်က input မတောင်းတော့ဘူး
+        //whenever we relate a number from the scanner, being an Int or Float or Double or some other type,
+        //we press the Enter key on the keyboard as we just did with the age.
+        //So doing that ends the line
+        //to handle the Enter key issue, we have to call next line, so that the scanner works as expected
+        //Now internally the scanner is checking for a line separator
+        //when we hit Enter we're typing a line separator so that's interpreted as input
+        //When we reach the next line method that retrieves the name
+        //the input for a name becomes Enter, so is essentially skipped.
+        scanner.nextLine(); //handle next line character
+
         System.out.println("Enter your name:");
         String name = scanner.nextLine();
 
         System.out.println("Your name is " + name + " and you are " + age + " years old");
+
+        scanner.close();
     }
 }
